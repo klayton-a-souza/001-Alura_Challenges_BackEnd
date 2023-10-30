60c8c3595d53870b9362890895e8606c76a2d65c
Klayton Souza <klayton.al.souza@gmail.com>
2023-10-30 15:02:32 -0300

    Código 26: Autenticação - Validando o token recebido

--- src/main/java/aluraFlix/API/security/TokenService.java
+++ src/main/java/aluraFlix/API/security/TokenService.java
@@ -4,6 +4,7 @@ import aluraFlix.API.model.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,19 @@ public class TokenService {
                     .sign(algoritimo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject (String tokenJWT){
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    .withIssuer("API AluraFlix")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido ou expirado!");
         }
     }
 
